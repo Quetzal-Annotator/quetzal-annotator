56a4d8da7fd52f69e62af64a79d3941449c95048
Eric Deutsch <ericdeutsch@gmail.com>
2025-03-11 20:46:12 -0700

    Move the testing stuff to the pytest files

--- quetzal-annotator/universal_spectrum_identifier_validator.py
+++ quetzal-annotator/universal_spectrum_identifier_validator.py
@@ -71,135 +71,41 @@ class UniversalSpectrumIdentifierValidator(object):
         return self
 
 
-# If this class is run from the command line, perform a short little test to see if it is working correctly
-def run_tests():
-    test_usis = [
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951" ],
-        [ "invalid", "PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [ "invalid", "mzspec:PASS002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [ "invalid", None ],
-        [ "invalid", 3 ],
-        [ "invalid", "mzspec" ],
-        [ "invalid", "mzspec:" ],
-        [ "invalid", "mzspec:PXD001234" ],
-        [ "invalid", "mzspec:PXD001234:00261_A06_P001564_B00E_A00_R1:scan" ],
-        [   "valid", "mzspec:PXD001234:00261_A06_P001564_B00E_A00_R1:index:10951" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[+79]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[UNIMOD:34]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD001234:Dilution1:4:scan:10951"],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:test1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1\\:test1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2:PA-28732" ],
-        [   "valid", "mzspec:PXD001234:[Control]fr10:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control[2]]fr10:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control]fr10[7]:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control[2]]fr10[7]:scan:10951" ],
-    ]
-    test_usisValid = []
 
-    # Loop over each test USI, parse it, and determine if it is valid or not, and print the index number
-    print("Testing example USIs:")
-    for usiSet in test_usis:
-        expectedStatus = usiSet[0]
-        usi_string = usiSet[1]
-
-        # Create a new UniversalSpectrumIdentifier object
-        # made the USI object itself take a string so that parse does not need to be called explicitly
-        usi = UniversalSpectrumIdentifier(usi_string)
-        expected_validity = True
-        if expectedStatus == 'invalid':
-            expected_validity = False
-        else:
-            expectedStatus = 'valid  '
-
-        status = 'PASS'
-        if usi.is_valid is not expected_validity:
-            status = 'FAIL'
-
-        response = usi.is_valid
-        test_usisValid.append(response)
-        print(f"{status}\texpected {expectedStatus}\t{usi_string}")
-    # check to see if parsing is correct
-    #print(test_usisValid)
+    def get_example_valid_usi_list(self):
+        example_usis = [
+            "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951",
+            "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2",
+            "mzspec:PXD001234:00261_A06_P001564_B00E_A00_R1:index:10951",
+            "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2",
+            "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[+79]IDELVISK/2",
+            "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[UNIMOD:34]IDELVISK/2",
+            "mzspec:PXD001234:Dilution1:4:scan:10951",
+            "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:test1:scan:10951:PEPT[Phospho]IDELVISK/2",
+            "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1\\:test1:scan:10951:PEPT[Phospho]IDELVISK/2",
+            "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2:PA-28732",
+            "mzspec:PXD001234:[Control]fr10:scan:10951",
+            "mzspec:PXD001234:[Control[2]]fr10:scan:10951",
+            "mzspec:PXD001234:[Control]fr10[7]:scan:10951",
+            "mzspec:PXD001234:[Control[2]]fr10[7]:scan:10951"
+        ]
+        return example_usis
 
 
-#### Validate one of several testing USIs for testing purposes
-def run_one_test():
-    test_usis = [
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951" ],
-        [ "invalid", "PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [ "invalid", "mzspec:PASS002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [ "invalid", None ],
-        [ "invalid", 3 ],
-        [ "invalid", "mzspec" ],
-        [ "invalid", "mzspec:" ],
-        [ "invalid", "mzspec:PXD001234" ],
-        [ "invalid", "mzspec:PXD001234:00261_A06_P001564_B00E_A00_R1:scan" ],
-        [   "valid", "mzspec:PXD001234:00261_A06_P001564_B00E_A00_R1:index:10951" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[+79]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[UNIMOD:34]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD001234:Dilution1:4:scan:10951"],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:test1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1\\:test1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2:PA-28732" ],
-        [   "valid", "mzspec:PXD001234:[Control]fr10:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control[2]]fr10:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control]fr10[7]:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control[2]]fr10[7]:scan:10951" ],
-    ]
- 
-    usi_string = test_usis[1][1]
 
-    usi_validator = UniversalSpectrumIdentifierValidator()
-    options = {}
+    def get_example_invalid_usi_list(self):
+        example_usis = [
+            "PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951",
+            "mzspec:PASS002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2",
+            None,
+            3,
+            "mzspec",
+            "mzspec:",
+            "mzspec:PXD001234",
+            "mzspec:PXD001234:00261_A06_P001564_B00E_A00_R1:scan"
+        ]
+        return example_usis
 
-    usi_validator.validate_usi_list( [usi_string], options, verbose=1)
-    print('==== Result:')
-    print(json.dumps(usi_validator.response,sort_keys=True,indent=2))
-
-
-#### Validate the whole list of testing USIs in one batch
-def run_list_test():
-    test_usis = [
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951" ],
-        [ "invalid", "PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [ "invalid", "mzspec:PASS002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [ "invalid", None ],
-        [ "invalid", 3 ],
-        [ "invalid", "mzspec" ],
-        [ "invalid", "mzspec:" ],
-        [ "invalid", "mzspec:PXD001234" ],
-        [ "invalid", "mzspec:PXD001234:00261_A06_P001564_B00E_A00_R1:scan" ],
-        [   "valid", "mzspec:PXD001234:00261_A06_P001564_B00E_A00_R1:index:10951" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[+79]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[UNIMOD:34]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD001234:Dilution1:4:scan:10951"],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:test1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1\\:test1:scan:10951:PEPT[Phospho]IDELVISK/2" ],
-        [   "valid", "mzspec:PXD002437:00261_A06_P001564_B00E_A00_R1:scan:10951:PEPT[Phospho]IDELVISK/2:PA-28732" ],
-        [   "valid", "mzspec:PXD001234:[Control]fr10:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control[2]]fr10:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control]fr10[7]:scan:10951" ],
-        [   "valid", "mzspec:PXD001234:[Control[2]]fr10[7]:scan:10951" ],
-    ]
- 
-    usi_list = []
-    for row in test_usis:
-        if row[1] is not None and isinstance(row[1],str):
-            usi_list.append(row[1])
-
-    usi_validator = UniversalSpectrumIdentifierValidator()
-    options = {}
-
-    usi_validator.validate_usi_list( usi_list, options)
-    print('==== Result:')
-    print(json.dumps(usi_validator.response,sort_keys=True,indent=2))
 
 
 #### A very simple example of using this class
@@ -209,11 +115,9 @@ def example():
     print(json.dumps(usi_validator.response,sort_keys=True,indent=2))
 
 
-#### If class is invoked directly
+
+#### Direct command-line invocation
 def main():
     example()
-    #run_one_test()
-    #run_list_test()
-
 
 if __name__ == "__main__": main()
